a4975f32c747780d531245012c98ffb39412fbac
SLCoders <45494512+SLCoders@users.noreply.github.com>
2019-01-07 20:28:35 +0530

    0.2.0.1.5
    contact email sennding sucssess !!
    
    Co-Authored-By: yassaperera <yassaperera@users.noreply.github.com>

--- php/contact.php
+++ php/contact.php
@@ -1,3 +1,15 @@
 <?php
-    var_dump($_POST);
+    
+    $to = 'info@gixez.com';
+    $subject = $_POST['subject'];
+    $message =  'Customer Message'."\n". $_POST['txt'];
+    $contact =  'Customer Name - '.$_POST['name']."\n".
+                'Customer Phone - '.$_POST['phone']."\n".
+                'Customer Email - '.$_POST['email'];
+    
+   
+    mail($to,$subject,$message,$contact);
+  
+    
+   
 ?>
